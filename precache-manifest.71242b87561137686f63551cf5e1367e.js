b5be0994deb1fd7b2c9cb131ba7dae6e0d9ee1b0
Julia Clements <tiiedye@gmail.com>
2020-06-25 16:47:53 -0600

    Updates

---
+++ precache-manifest.71242b87561137686f63551cf5e1367e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554639cb1bb0003fcf528300bb4a181b",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "b4c807a340523c1ff4df",
+    "url": "/React-Portfolio/static/css/main.01c35f22.chunk.css"
+  },
+  {
+    "revision": "98a636434aa39987b07a",
+    "url": "/React-Portfolio/static/js/2.d2095a1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.d2095a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c807a340523c1ff4df",
+    "url": "/React-Portfolio/static/js/main.e5ceb4ea.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "c607d7b7d4539d8641b2d63f211b3e1f",
+    "url": "/React-Portfolio/static/media/Hero.c607d7b7.jpg"
+  },
+  {
+    "revision": "71d3f5090543218bfa41387614c95fbf",
+    "url": "/React-Portfolio/static/media/ProfilePic.71d3f509.jpg"
+  },
+  {
+    "revision": "6ee8d9ea2c245340a3774bce9ece45e5",
+    "url": "/React-Portfolio/static/media/Selfcare-sidekick.6ee8d9ea.png"
+  },
+  {
+    "revision": "ba1d81f951c9edb01b73e0aab37aa8f2",
+    "url": "/React-Portfolio/static/media/foodies-and-movies-thumb.ba1d81f9.png"
+  }
+]);
