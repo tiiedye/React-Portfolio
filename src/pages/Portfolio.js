a135bbff1b03b057b1516a4aa8102d4726ae7b62
tiiedye <tiiedye@gmail.com>
2020-09-04 14:14:00 -0700

    updating portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -164,6 +164,69 @@ function Portfolio() {
 
                 <div className="text-center">
                     <p>
+                        <h5>Simple Bank App</h5>
+                        <img src={require("../assets/simple-bank-app.png")} alt="CLI" style={styles.other}></img>
+                        <br />  
+                        using a super simple banking app to play around in Visual Studio. Learning class Inheritance, as well as how to organize VS projects/solutions and integrate with GitHub.
+                        <br />
+                        <a class="a" href="https://github.com/tiiedye/Simple-Bank-App" target="_blank" rel="noopener noreferrer">Repo</a>
+                    </p>
+
+                    <p>
+                        <h5>Overloading-Operators 2.0</h5>
+                        <img src={require("../assets/overloading-operators-2.png")} alt="CLI" style={styles.other}></img>
+                        <br />  
+                        Practice overloading operators using NON-MEMBER METHODS.
+                        <br />
+                        <a class="a" href="https://github.com/tiiedye/Overloading-Operators-2.0" target="_blank" rel="noopener noreferrer">Repo</a>
+                    </p>
+
+                    <p>
+                        <h5>Overloading-Operators</h5>
+                        <img src={require("../assets/overloading-operators.png")} alt="CLI" style={styles.other}></img>
+                        <br />  
+                        Practice overloading operators using MEMBER METHODS.
+                        <br />
+                        <a class="a" href="https://github.com/tiiedye/Overloading-Operators" target="_blank" rel="noopener noreferrer">Repo</a>
+                    </p>
+
+                    <p>
+                        <h5>Movie Listings</h5>
+                        <img src={require("../assets/movie-listings.png")} alt="CLI" style={styles.other}></img>
+                        <br />  
+                        a simple app that adds movies to a list and prints out their title, rating, and how many time's you've viewed them.
+                        <br />
+                        <a class="a" href="https://github.com/tiiedye/Movie-Listings" target="_blank" rel="noopener noreferrer">Repo</a>
+                    </p>
+
+                    <p>
+                        <h5>Playing With Pointers</h5>
+                        <img src={require("../assets/playing-with-pointers.png")} alt="CLI" style={styles.other}></img>
+                        <br />  
+                        A really simple pointer exercise that multiplies the values in one array by the values in another array, and creates a third array that holds the results. The program then displays all three arrays.
+                        <br />
+                        <a class="a" href="https://github.com/tiiedye/playing-with-pointers" target="_blank" rel="noopener noreferrer">Repo</a>
+                    </p>
+
+                    <p>
+                        <h5>Cleaning App</h5>
+                        <img src={require("../assets/cleaning-app.png")} alt="CLI" style={styles.other}></img>
+                        <br />  
+                        simple example carpet cleaning app using CLI and C++ that determines the cost of cleaning both small and large rooms.
+                        <br />
+                        <a class="a" href="https://github.com/tiiedye/simple-cleaning-app" target="_blank" rel="noopener noreferrer">Repo</a>
+                    </p>
+
+                    <p>
+                        <h5>Letter Pyramid</h5>
+                        <img src={require("../assets/letter-pyramid.png")} alt="CLI" style={styles.other}></img>
+                        <br />  
+                        A simple CLI program using C++ to create a pyramid out of the letters a user inputs.
+                        <br />
+                        <a class="a" href="https://github.com/tiiedye/Letter-Pyramid" target="_blank" rel="noopener noreferrer">Repo</a>
+                    </p>
+
+                    <p>
                         <h5>CLI Cipher With Strings App</h5>
                         <img src={require("../assets/cipher-with-strings.png")} alt="CLI" style={styles.other}></img>
                         <br />  
